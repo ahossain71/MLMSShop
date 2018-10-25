065a6c679e30a89091870e2674db0612b48b06d3
Azfar Hossain <azfar.hossain@yahoo.com>
2018-10-24 22:37:37 -0400

    modified paths in build-DEV.sh

--- buildscripts/build-DEV.sh
+++ buildscripts/build-DEV.sh
@@ -1,1 +1,1 @@
-ant -f ~/buildscripts/build.xml -propertyfile ~/buildscripts/build-DEV.properties
+ant -f ~/workspace/MLMS_Master_Build/buildscripts/build.xml -propertyfile ~/workspace/MLMS_Master_Build/buildscripts/build-DEV.properties
